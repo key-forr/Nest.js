d3e355d94732053ecff4c8ff628a60c60b417f03
key-forr <denyskifor@gmail.com>
2025-06-14 16:48:15 +0300

    requests, controllers, modules, services

---
+++ src/task/task.controller.ts
@@ -0,0 +1,34 @@
+import { Body, Controller, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
+
+@Controller('task')
+export class TaskController {
+  constructor(private readonly taskService: TaskService) {}
+
+  @Get('all')
+  findAll() {
+    return this.taskService.findAll();
+  }
+
+  @Get('id/:id')
+  findById(@Param('id') id: string) {
+    return this.taskService.findById(+id);
+  }
+
+  @Post()
+  create(@Body() dto: CreateTaskDto) {
+    return this.taskService.create(dto);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() dto: UpdateTaskDto) {
+    return this.taskService.update(+id, dto);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') id: string, @Body() dto: Partial<UpdateTaskDto>) {
+    return this.taskService.patch(+id, dto);
+  }
+}
